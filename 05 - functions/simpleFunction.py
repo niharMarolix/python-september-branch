5ea4856dd4399eaafa4b52556c536bef381eb292
“niharMarolix” <nihar@marolix.com>
2023-09-28 15:08:37 +0530

    function SI example

---
+++ 05 - functions/simpleFunction.py
@@ -0,0 +1,11 @@
+def siAndAmount(principal, rateOfInterest, timeInYears): # the values passed inside the functions are called as paramters or positional arguements
+    SI = ((principal*rateOfInterest*timeInYears))/100
+    Amount = SI+principal
+    print(int(Amount))
+
+
+p = 10000
+r = 8
+t = 10
+siAndAmount(p,r,t) # calling the siAndAmount function
+# when we are calling a function then the passes value are arguements
