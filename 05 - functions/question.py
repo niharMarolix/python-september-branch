2dd4ea342171063714945923f27d27c492eea5af
“niharMarolix” <nihar@marolix.com>
2023-09-29 14:51:15 +0530

    function tuts

---
+++ 05 - functions/question.py
@@ -0,0 +1,25 @@
+'''
+write a python function to print the follwoing:
+when age >  100 - aapka ek per kabar mai hai
+when age 80-100 - acha jee liye
+when age is < 80 - abhi toh aur jina hai
+
+age = int
+'''
+
+age = int(input("please enter your age"))
+
+def Jīvin̄caḍāniki(kndkjvn):
+    if kndkjvn > 100:
+        print("aapka ek per kabar mai hai")
+    elif 80<kndkjvn>100:
+        print("acha jee liye")
+    
+    elif 1<kndkjvn<80:
+        print(" abhi toh aur jina hai")
+    else:
+        print("sit home ccok rice")
+
+
+Jīvin̄caḍāniki(age)
+
---
+++ 05 - functions/randomSnippet.py
@@ -0,0 +1,20 @@
+def greet(a):
+    a = "hey bro, \n"
+    b = "how you doing?"
+    print(a+b)
+
+
+a = "hey man, what's up!"
+greet(a)
+print(a)
+(greet(a))
+
+'''
+HI Bro,
+how you doing?
+
+hey man, what's up!
+
+HI Bro,
+how you doing?
+'''
